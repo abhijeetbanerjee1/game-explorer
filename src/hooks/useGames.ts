b92438b52123ddf3fb2a1fb1ef7fb04eeed8033f
abhijeetbanerjee1 <abhijeet.banerjee883@gmail.com>
2024-03-13 17:35:13 -0400

    Displaying Platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
-import { Container } from "@chakra-ui/react";
 import { CanceledError } from "axios";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGameResponse {
