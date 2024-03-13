5e0493457abd3d4a7250b44922195e6ebfca874a
abhijeetbanerjee1 <abhijeet.banerjee883@gmail.com>
2024-03-13 16:49:52 -0400

    Building Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import apiClient from "../services/api-client";
 import { Container } from "@chakra-ui/react";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGameResponse {
