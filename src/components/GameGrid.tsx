bb381dd6083cc293bfdc04621fc5cb0fcf7a743e
abhijeetbanerjee1 <abhijeet.banerjee883@gmail.com>
2024-03-13 20:16:47 -0400

    Created Generic Fetching Hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,9 +3,10 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import useData from "../hooks/useData";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -23,7 +24,7 @@ const GameGrid = () => {
             // </GameCardContainer>
             <GameCardSkeleton key={skeleton} />
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           //   <GameCardContainer>
           //     <GameCard key={game.id} game={game} />
           //   </GameCardContainer>
