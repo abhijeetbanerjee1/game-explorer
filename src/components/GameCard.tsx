48806d898ebaa1ad2cf6cae5a27f82b56317d28f
abhijeetbanerjee1 <abhijeet.banerjee883@gmail.com>
2024-04-16 21:42:14 -0400

    Fetch Game Details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,29 +11,30 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageURL from "../services/image-url";
 import Emojis from "./Emojis";
+import { Link } from "react-router-dom";
 
 export interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
-    <Card borderRadius={10} overflow="hidden" onClick={onOpen} cursor="pointer">
-      <Image src={getCroppedImageURL(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          {game.name}
-          <Emojis rating={game.rating_top} />
-        </Heading>
-      </CardBody>
+    <Card>
+      <Link to={"/games/" + game.slug}>
+        <Image src={getCroppedImageURL(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">
+            {game.name}
+            <Emojis rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Link>
     </Card>
   );
 };
