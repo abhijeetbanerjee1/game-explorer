cfaa3aef4ce1cef4a109e860946fb6d3fcf2efc2
abhijeetbanerjee1 <abhijeet.banerjee883@gmail.com>
2024-04-17 15:20:03 -0400

    Fixed Nav Bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,15 @@
-import { HStack, Image, Box } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Image src={logo} boxSize="65px" objectFit="contain" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
